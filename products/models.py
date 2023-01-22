48860783634928e5ddeabacb2ab3a9ed0dc79e8e
andreisukhar <12onlyfortoday12@gmail.com>
2023-01-22 15:09:24 +0700

    One more

--- products/models.py
+++ products/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+# models = таблица = модели
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.id} | Категория: {self.name}'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='products_images')
+    category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.id} | Продукт: {self.name} | Категория: {self.category.name}'
--- products/views.py
+++ products/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from products.models import ProductCategory, Product
 
-
+# функции = контроллеры = вьюхи
 def index(request):
-    return render(request, 'products/index.html')
+    context = {
+        'title': 'Store',
+    }
+    return render(request, 'products/index.html', context)
 
 
 def products(request):
-    return render(request, 'products/products.html')
+    context = {
+        'title': 'Store - Каталог',
+        'products': Product.objects.all(),
+        'categories': ProductCategory.objects.all(),
+    }
+    return render(request, 'products/products.html', context)
