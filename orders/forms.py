c5ca0acdaaf666af3b9e89050b7f8180676a379b
Andrei <12onlyfortoday12@gamil.com>
2023-02-21 15:07:52 +0700

    add order create view, add humanize

--- orders/forms.py
+++ orders/forms.py
@@ -4,6 +4,13 @@ from orders.models import Order
 
 
 class OrderForm(forms.ModelForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Иван'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Иванов'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'mail@example.ru'}))
+    address = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control', 'placeholder': 'Россия, Москва, ул. Мира, д. 6, кв. 10'
+    }))
+
     class Meta:
         model = Order
         fields = ('first_name', 'last_name', 'email', 'address')
