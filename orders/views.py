16946db0f5ac683cf4d29704ea361f069cd96960
Andrei <12onlyfortoday12@gamil.com>
2023-02-22 11:16:42 +0700

    add products to stripe, add products to order create

--- orders/views.py
+++ orders/views.py
@@ -10,7 +10,7 @@ from django.conf import settings
 
 from common.views import TitleMixin
 from orders.forms import OrderForm
-
+from products.models import Basket
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
@@ -32,13 +32,9 @@ class OrderCreateView(TitleMixin, CreateView):
 
     def post(self, request, *args, **kwargs):
         super(OrderCreateView, self).post(request, *args, **kwargs)
+        baskets = Basket.objects.filter(user=self.request.user)
         checkout_session = stripe.checkout.Session.create(
-            line_items=[
-                {
-                    'price': 'price_1MdrHsKF9blkfTdF3Lc16jgs',
-                    'quantity': 1,
-                },
-            ],
+            line_items=baskets.stripe_products(),
             metadata={'order_id': self.object.id},
             mode='payment',
             success_url='{}{}'.format(settings.DOMAIN_NAME, reverse('orders:order_success')),
