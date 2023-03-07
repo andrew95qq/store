2e17b1cfa4dccd7d66e5ea29aebcf66b24d9bba9
Andrei <12onlyfortoday12@gamil.com>
2023-03-07 15:56:59 +0700

    settings update

--- store/settings.py
+++ store/settings.py
@@ -182,9 +182,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = (BASE_DIR / 'static',)
-
-if not DEBUG:
+if DEBUG:
+    STATICFILES_DIRS = (BASE_DIR / 'static',)
+else:
     STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
