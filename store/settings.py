b31b2cd9353ad5d0479726829e278f66822ea4dc
Andrei <12onlyfortoday12@gamil.com>
2023-03-07 15:34:55 +0700

    settings update

--- store/settings.py
+++ store/settings.py
@@ -184,6 +184,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (BASE_DIR / 'static',)
 
+if not DEBUG:
+    STATIC_ROOT = BASE_DIR / 'static'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
